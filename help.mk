98af5cadf1e4ee24344648c0132ca2c72a3cf266
Mark A. Hershberger <mah@nichework.com>
2019-05-12 16:10:47 -0400

    Copyright headers for almost everyone!

--- help.mk
+++ help.mk
@@ -1,6 +1,21 @@
 #-*-tab-width: 4; fill-column: 68; whitespace-line-column: 69 -*-
 # vi:shiftwidth=4 tabstop=4 textwidth=68
 
+# Copyright (C) 2019  Wikimedia Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Makefile help based on
 # https://github.com/ianstormtaylor/makefile-help
 .PHONY: help
