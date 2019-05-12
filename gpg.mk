edbf93f7150adbf739f1f4efc303d4541b61af22
Mark A. Hershberger <mah@nichework.com>
2019-05-12 16:10:47 -0400

    Copyright headers for almost everyone!

--- gpg.mk
+++ gpg.mk
@@ -1,6 +1,21 @@
 #-*-tab-width: 4; fill-column: 76; whitespace-line-column: 77 -*-
 # vi:shiftwidth=4 tabstop=4 textwidth=76
 
+# Copyright (C) 2019  Wikimedia Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 keyUrl=https://www.mediawiki.org/keys/keys.html
 gpgDir=${workDir}/gpg
 myGpg=$(shell HOME=${oldHOME} gpgconf --list-dirs |							\
--- src/Branch.php
+++ src/Branch.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2019  Mark A. Hershberger
+ * Copyright (C) 2019  Wikimedia Foundation, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Mark A. Hershberger <mah@nichework.com>
  */
 
 namespace Wikimedia\Release;
