91c3937a0e7c99a75a11817d8f7093a54a55d96d
Mark A. Hershberger <mah@nichework.com>
2019-05-11 22:55:39 -0400

    Start differentiating the WMF and tarball branches
    * Add my emacs cruft
    * Add composer.lock
    * Update .gitignore
    * Try some type hinting
    * phpdoc
    * This has “worked” with tarball test and REL1_33

--- src/Branch/Wmf.php
+++ src/Branch/Wmf.php
@@ -5,7 +5,23 @@ namespace Wikimedia\Release\Branch;
 use Wikimedia\Release\Branch;
 
 class Wmf extends Branch {
-	public static function getDescription() {
+	public static function getDescription() :string {
 		return 'Create a WMF Branch';
+	}
+
+	protected function getWorkDir() :string {
+		return sys_get_temp_dir() . '/make-wmf-branch';
+	}
+
+	protected function getBranchPrefix() :string {
+		return "wmf/";
+	}
+
+	protected function getRepoPath() :string {
+		return 'https://gerrit.wikimedia.org/r/mediawiki';
+	}
+
+	protected function getBranchDir() :string {
+		return 'wmf';
 	}
 }
